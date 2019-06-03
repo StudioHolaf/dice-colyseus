27304fa383a9bcd31d81ea6a7700e1985098a9d7
Thibaut De Lafforest <tdelafforest@terredesienne.com>
2019-06-03 19:02:15 +0200

    Adding DemoRoom

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import { monitor } from '@colyseus/monitor';
 
 // Import demo room handlers
 import { ChatRoom } from "./rooms/01-chat-room";
+import { DemoRoom } from "./rooms/DemoRoom";
 import { StateHandlerRoom } from "./rooms/02-state-handler";
 import { AuthRoom } from "./rooms/03-auth";
 import { CreateOrJoinRoom } from "./rooms/04-create-or-join-room";
@@ -21,6 +22,9 @@ const gameServer = new Server({
 
 // Register ChatRoom as "chat"
 gameServer.register("chat", ChatRoom);
+
+// Register DemoRoom as "demo"
+gameServer.register("demo", DemoRoom);
 
 // Register ChatRoom with initial options, as "chat_with_options"
 // onInit(options) will receive client join options + options registered here.
---
+++ rooms/DemoRoom.ts
@@ -0,0 +1,104 @@
+import { Room, Client, generateId } from "colyseus";
+import { Schema, type, MapSchema, ArraySchema } from "@colyseus/schema";
+import { verifyToken, User, IUser } from "@colyseus/social";
+
+class Entity extends Schema {
+  @type("number")
+  x: number = 0;
+
+  @type("number")
+  y: number = 0;
+}
+
+class Player extends Entity {
+  @type("boolean")
+  connected: boolean = true;
+}
+
+class Enemy extends Entity {
+  @type("number")
+  power: number = Math.random() * 10;
+}
+
+class State extends Schema {
+  @type({ map: Entity })
+  entities = new MapSchema<Entity>();
+
+  @type(["number"])
+  arrayOfNumbers = new ArraySchema<number>();
+}
+
+export class DemoRoom extends Room {
+
+  onInit (options: any) {
+    console.log("DemoRoom created!", options);
+
+    this.setState(new State());
+    this.populateEnemies();
+
+    this.setPatchRate(1000 / 20);
+    this.setSimulationInterval((dt) => this.update(dt));
+  }
+
+  async onAuth (options) {
+    return await User.findById(verifyToken(options.token)._id);
+  }
+
+  populateEnemies () {
+    for (let i=0; i<=3; i++) {
+      const enemy = new Enemy();
+      enemy.x = Math.random() * 2;
+      enemy.y = Math.random() * 2;
+      this.state.entities[generateId()] = enemy;
+      this.state.arrayOfNumbers.push(Math.random());
+    }
+  }
+
+  requestJoin (options: any) {
+    console.log("request join!", options);
+    return true;
+  }
+
+  onJoin (client: Client, options: any, user: IUser) {
+    console.log("client joined!", client.sessionId);
+    this.state.entities[client.sessionId] = new Player();
+  }
+
+  async onLeave (client: Client, consented: boolean) {
+    this.state.entities[client.sessionId].connected = false;
+
+    try {
+      if (consented) {
+        throw new Error("consented leave!");
+      }
+
+      console.log("let's wait for reconnection!")
+      const newClient = await this.allowReconnection(client, 10);
+      console.log("reconnected!", newClient.sessionId);
+
+    } catch (e) {
+      console.log("disconnected!", client.sessionId);
+      delete this.state.entities[client.sessionId];
+    }
+  }
+
+  onMessage (client: Client, data: any) {
+    console.log(data, "received from", client.sessionId);
+
+    if (data === "move_right") {
+      this.state.entities[client.sessionId].x += 0.01;
+    }
+    console.log(this.state.entities[client.sessionId].x);
+
+    this.broadcast({ hello: "hello world" });
+  }
+
+  update (dt?: number) {
+    // console.log("num clients:", Object.keys(this.clients).length);
+  }
+
+  onDispose () {
+    console.log("disposing DemoRoom...");
+  }
+
+}
