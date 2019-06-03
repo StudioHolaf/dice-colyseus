b5a81447497db9a09d1c82430cae10c2489f5170
Thibaut De Lafforest <tdelafforest@terredesienne.com>
2019-06-03 19:14:59 +0200

    Testing index.ts from demo

--- index.ts
+++ index.ts
@@ -1,61 +1,37 @@
-import path from 'path';
-import express from 'express';
-import serveIndex from 'serve-index';
-import { createServer } from 'http';
-import { Server } from 'colyseus';
-import { monitor } from '@colyseus/monitor';
+import http from "http";
+import express from "express";
 
-// Import demo room handlers
-import { ChatRoom } from "./rooms/01-chat-room";
-import { DemoRoom } from "./rooms/DemoRoom";
-import { StateHandlerRoom } from "./rooms/02-state-handler";
-import { AuthRoom } from "./rooms/03-auth";
-import { CreateOrJoinRoom } from "./rooms/04-create-or-join-room";
+import { Server, serialize, FossilDeltaSerializer } from "colyseus";
+import { DemoRoom } from "./DemoRoom";
 
-const port = Number(process.env.PORT || 2567);
+import socialRoutes from "@colyseus/social/express";
+
+const PORT = Number(process.env.PORT || 2567);
+
 const app = express();
-
-// Attach WebSocket Server on HTTP Server.
 const gameServer = new Server({
-  server: createServer(app)
+    server: http.createServer(app),
+    pingTimeout: 0
 });
-
-// Register ChatRoom as "chat"
-gameServer.register("chat", ChatRoom);
 
 // Register DemoRoom as "demo"
 gameServer.register("demo", DemoRoom);
 
-// Register ChatRoom with initial options, as "chat_with_options"
-// onInit(options) will receive client join options + options registered here.
-gameServer.register("chat_with_options", ChatRoom, {
-    custom_options: "you can use me on Room#onInit"
+/**
+ * FossilDelta demo (deprecated)
+ */
+@serialize(FossilDeltaSerializer)
+class DemoRoomFossilDelta extends DemoRoom {}
+gameServer.register("demo_fossil", DemoRoomFossilDelta);
+
+app.use("/", socialRoutes);
+
+app.get("/something", function (req, res) {
+    console.log("something!", process.pid);
+    res.send("Hey!");
 });
 
-// Register StateHandlerRoom as "state_handler"
-gameServer.register("state_handler", StateHandlerRoom);
+// Listen on specified PORT number
+gameServer.listen(PORT);
 
-// Register StateHandlerRoom as "state_handler"
-gameServer.register("auth", AuthRoom);
-
-// Register CreateOrJoin as "create_or_join"
-gameServer.register("create_or_join", CreateOrJoinRoom);
-
-app.use('/', express.static(path.join(__dirname, "static")));
-app.use('/', serveIndex(path.join(__dirname, "static"), {'icons': true}))
-
-// (optional) attach web monitoring panel
-app.use('/colyseus', monitor(gameServer));
-
-gameServer.onShutdown(function(){
-  console.log(`game server is going down.`);
-});
-
-gameServer.listen(port);
-
-// process.on("uncaughtException", (e) => {
-//   console.log(e.stack);
-//   process.exit(1);
-// });
-
-console.log(`Listening on http://localhost:${ port }`);
+console.log("Running on ws://localhost:" + PORT);
