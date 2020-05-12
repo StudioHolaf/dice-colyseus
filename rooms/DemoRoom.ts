2de7baefb927ddfe98def4d5806ea479c414f79d
Anton "Algost" Bodin Marty <antbo17@hotmail.fr>
2020-05-12 14:58:25 +0200

    forgot the splice for [ and ]

--- rooms/DemoRoom.ts
+++ rooms/DemoRoom.ts
@@ -611,8 +611,8 @@ var i = this.LobbyClients.length;
 while (i--) {
         this.LobbyClients.splice(i, 1);
     }
-    this.LobbyClients(this.LobbyClients.length - 1, 1); // premier ]
-    this.LobbyClients(0, 1); // premier [
+    this.LobbyClients.splice(this.LobbyClients.length - 1, 1); // premier ]
+    this.LobbyClients.splice(0, 1); // premier [
     this.LobbyClients.push(lobbyInfos);
         this.broadcast({
         LobbyClients: JSON.stringify(this.LobbyClients),
