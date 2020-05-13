073a4d4242ef4e65f37fa7537c16e7af8dc9a4ba
Anton "Algost" Bodin Marty <antbo17@hotmail.fr>
2020-05-13 14:59:45 +0200

    Update LobbyClient.ts

--- diceofolympus/LobbyClient.ts
+++ diceofolympus/LobbyClient.ts
@@ -1,14 +1,14 @@
 
 export class LobbyClient {
 
-    public clientID:number;
+    public clientID:any;
     public clientName:any;
     public clientPlayerID:any;
     public status:any;
     public isHost:boolean;
 
 
-    constructor (clientID: number , clientName: any, clientPlayerID: any, status: any, isHost: boolean) {
+    constructor (clientID: any , clientName: any, clientPlayerID: any, status: any, isHost: boolean) {
 	 this.clientID = clientID;
 	 this.clientName = clientName;
 	 this.clientPlayerID = clientPlayerID;
