5d6f99f993cc3f2a1657471fe51702672cf182b8
Anton "Algost" Bodin Marty <antbo17@hotmail.fr>
2020-07-10 16:17:06 +0200

    Update DemoRoom.ts

--- rooms/DemoRoom.ts
+++ rooms/DemoRoom.ts
@@ -311,9 +311,9 @@ export class DemoRoom extends Room {
                     this.resendDataTry = 0;
 
                     console.log("askServerForTirage dicesPossibilitiesJsoned : " + data.dicesPossibilitiesJsoned);
-                    console.log("possibilities : "+ data.dicesPossibilitiesJsoned.length[0]);
-                    console.log("possibilities 2 : "+ data.dicesPossibilitiesJsoned.length[0][1]);
-                    console.log("possibilities 3 : "+ data.dicesPossibilitiesJsoned.length[0][0].length);
+                    console.log("possibilities : "+ data.dicesPossibilitiesJsoned.length);
+                    console.log("possibilities 2 : "+ data.dicesPossibilitiesJsoned[0][1].length);
+                    console.log("possibilities 3 : "+ data.dicesPossibilitiesJsoned[0][0].length);
                     var rnd1 = Math.floor(Math.random() * data.dicesPossibilitiesJsoned[0].length); // le dé était sur 1 et n'a jamais changer en 15 rerolls
                     console.log("rnd1 : "+rnd1);
 
